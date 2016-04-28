52b0c364f22b06032f1e5de1263e5d759dbd43b0
ogama <ogama47500@gmail.com>
2016-04-28 21:52:13 +0200

    Initial commit for date.serial

--- app/types/date/serial.js
+++ app/types/date/serial.js
@@ -1,3 +1,104 @@
+var check = require('check-type');
+var extend = require('extend');
+
+var AbstractType = require('../abstract.js');
+
 module.exports = function (options) {
-    throw new Error('Not implemented');                
+        
+    if (!check(options).has('from')
+        || !check(options).has('next') 
+        || !check(options).has('next.interval') 
+        || !check(options).has('next.increment')) {
+        
+        throw new Error('Options for date.serial must match {from:date,to:date (default: infinity),next{interval:"year"|"month"|"day"|"hour"|"minute"|"second",increment:integer},cycle:boolean (default: true)}');
+    } else {
+        
+        if (!check(options.from).is('date')) {
+            
+            throw new Error('from is not a date');
+        }
+        
+        if (!check(options.next.interval).is('string')) {
+            
+            throw new Error('next.interval is not a string');
+        }
+        
+        if (!check(options.next.increment).is('number')) {
+            
+            throw new Error('next.increment is not a number');
+        }
+        
+        if (options.next.interval.toLowerCase() != 'year'
+           && options.next.interval.toLowerCase() != 'month'
+           && options.next.interval.toLowerCase() != 'day'
+           && options.next.interval.toLowerCase() != 'hour'
+           && options.next.interval.toLowerCase() != 'minute'
+           && options.next.interval.toLowerCase() != 'second'
+           && options.next.interval.toLowerCase() != 'millisecond') {
+            
+            throw new Error('next.interval value is incorrect : "' + options.next.interval + '". Expected "year" or "month" or"day" or "hour" or "minute" or "second" or "millisecond"');
+        }
+    }
+
+    if (!check(options).has('to')) {
+        options.to = null;
+    }
+    
+    if (!check(options).has('cycle')) {
+        options.cycle = true;
+    }
+
+    return extend({}, AbstractType, {        
+
+        min: options.from,
+        max: options.to,
+        next: options.from,
+        interval: options.next.interval.toLowerCase(),
+        increment: options.next.increment,
+        doCycle: options.cycle,
+        
+        getValue: function (model) {
+            
+            var valueToReturn = this.next;            
+            var futureValue = incrementDateBy(this.next, this.interval, this.increment);            
+            
+            var maxPlusOneIncrement = null;
+            
+            if (this.max != null) {
+                
+                maxPlusOneIncrement = incrementDateBy(this.max, this.interval, this.increment);
+            }
+            
+            if (maxPlusOneIncrement != null 
+                && futureValue.getTime() > maxPlusOneIncrement.getTime()
+                && !this.doCycle) {
+                                
+                throw new Error('Max increment. Add cycle: true to allow reloop');
+            } else if (maxPlusOneIncrement != null 
+                        && futureValue.getTime() > maxPlusOneIncrement.getTime()
+                        && this.doCycle) {
+                
+                this.next = this.min;
+            } else {                
+                
+                this.next = futureValue;
+            }
+            
+                
+            return valueToReturn;
+        }
+    });   
 };
+
+function incrementDateBy (date, interval, increment) {
+    
+    return new Date(
+        interval == 'year' ? date.getFullYear() + increment : date.getFullYear(),
+        interval == 'month' ? date.getMonth() + increment : date.getMonth(),
+        interval == 'day' ? date.getDate() + increment : date.getDate(),
+        interval == 'hour' ? date.getHours() + increment : date.getHours(),
+        interval == 'minute' ? date.getMinutes() + increment : date.getMinutes(),
+        interval == 'second' ? date.getSeconds() + increment : date.getSeconds(),
+        interval == 'millisecond' ? date.getMilliseconds() + increment : date.getMilliseconds()
+    );
+}
---
+++ test/dates.js
@@ -0,0 +1,186 @@
+var dgf = require('../');
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+
+describe('dgf.type.date', function () {
+   
+    describe('#serial', function () {
+    
+        it ('should create date type generator', function () {
+           
+            var dateType = dgf.types.date.serial({
+                from: new Date(2016, 0, 1, 0, 0, 0, 0),
+                next: {
+                    interval: 'day',
+                    increment: 1
+                }
+            });
+            
+            assert.isNotNull(dateType);
+            assert.property(dateType, '__GEN_TYPE_IND');
+            assert.isFunction(dateType.getValue);
+        });
+        
+        it ('should not create date type because options are missing', function () {
+           
+            // missing from 
+            expect(function () {
+                
+                var dateType = dgf.types.date.serial({
+                    next: {
+                        interval: 'day',
+                        increment: 1
+                    }
+                });
+            }).to.throw(Error, 'Options for date.serial must match {from:date,to:date (default: infinity),next{interval:"year"|"month"|"day"|"hour"|"minute"|"second",increment:integer},cycle:boolean (default: true)}');
+            
+            // Missing next
+            expect(function () {
+                
+                var dateType = dgf.types.date.serial({
+                    from: new Date(2016, 0, 1, 0, 0, 0, 0)
+                });
+            }).to.throw(Error, 'Options for date.serial must match {from:date,to:date (default: infinity),next{interval:"year"|"month"|"day"|"hour"|"minute"|"second",increment:integer},cycle:boolean (default: true)}');
+            
+            // missing interval
+            expect(function () {
+                
+                var dateType = dgf.types.date.serial({
+                    from: new Date(2016, 0, 1, 0, 0, 0, 0),
+                    next: {
+                        increment: 1
+                    }
+                });
+            }).to.throw(Error, 'Options for date.serial must match {from:date,to:date (default: infinity),next{interval:"year"|"month"|"day"|"hour"|"minute"|"second",increment:integer},cycle:boolean (default: true)}');
+            
+            // missing increment
+            expect(function () {
+                
+                var dateType = dgf.types.date.serial({
+                    from: new Date(2016, 0, 1, 0, 0, 0, 0),
+                    next: {
+                        interval: 'day'
+                    }
+                });
+            }).to.throw(Error, 'Options for date.serial must match {from:date,to:date (default: infinity),next{interval:"year"|"month"|"day"|"hour"|"minute"|"second",increment:integer},cycle:boolean (default: true)}');
+        });
+        
+        it ('should not create date type because from options are not in correct type', function () {
+           
+            expect(function () {
+                
+                var dateType = dgf.types.date.serial({
+                    from: 1,
+                    next: {
+                        interval: 'day',
+                        increment: 1
+                    }
+                });
+            }).to.throw(Error, 'from is not a date');
+            
+            expect(function () {
+                
+                var dateType = dgf.types.date.serial({
+                    from: new Date(2016, 0, 1, 0, 0, 0, 0),
+                    next: {
+                        interval: 1,
+                        increment: 1
+                    }
+                });
+            }).to.throw(Error, 'next.interval is not a string');
+            
+            expect(function () {
+                
+                var dateType = dgf.types.date.serial({
+                    from: new Date(2016, 0, 1, 0, 0, 0, 0),
+                    next: {
+                        interval: 'test',
+                        increment: 1
+                    }
+                });
+            }).to.throw(Error, 'next.interval value is incorrect : "test". Expected "year" or "month" or"day" or "hour" or "minute" or "second" or "millisecond"');
+            
+            expect(function () {
+                
+                var dateType = dgf.types.date.serial({
+                    from: new Date(2016, 0, 1, 0, 0, 0, 0),
+                    next: {
+                        interval: 'day',
+                        increment: 'test'
+                    }
+                });
+            }).to.throw(Error, 'next.increment is not a number');
+        });
+        
+        it ('should create date type generator', function () {
+           
+            var dateType = dgf.types.date.serial({
+                from: new Date(2016, 0, 1, 0, 0, 0, 0),
+                next: {
+                    interval: 'day',
+                    increment: 1
+                }
+            });
+            
+            assert.isNotNull(dateType);
+            assert.property(dateType, '__GEN_TYPE_IND');
+            assert.isFunction(dateType.getValue);
+        });
+        
+        it ('should return next date in order', function () {
+           
+            var dateType = dgf.types.date.serial({
+                from: new Date(2016, 0, 1, 0, 0, 0, 0),
+                next: {
+                    interval: 'day',
+                    increment: 1
+                }
+            });
+            
+            var model = dgf.newModel();
+            
+            var date1 = dateType.getValue(model);
+            var date2 = dateType.getValue(model);
+            var date3 = dateType.getValue(model);
+            
+            assert.equal(date1.getTime(), new Date(2016, 0, 1, 0, 0, 0, 0).getTime());
+            assert.equal(date2.getTime(), new Date(2016, 0, 2, 0, 0, 0, 0).getTime());
+            assert.equal(date3.getTime(), new Date(2016, 0, 3, 0, 0, 0, 0).getTime());
+        });
+        
+        
+        it ('should return next date in order and throw error when out of range', function () {
+           
+            var dateType = dgf.types.date.serial({
+                from: new Date(2016, 0, 1, 0, 0, 0, 0),
+                to: new Date(2016, 0, 5, 0, 0, 0, 0),
+                next: {
+                    interval: 'day',
+                    increment: 1
+                },
+                cycle: false
+            });
+            
+            var model = dgf.newModel();
+            
+            var date1 = dateType.getValue(model);
+            var date2 = dateType.getValue(model);
+            var date3 = dateType.getValue(model);
+            var date4 = dateType.getValue(model);
+            var date5 = dateType.getValue(model);
+            
+            assert.equal(date1.getTime(), new Date(2016, 0, 1, 0, 0, 0, 0).getTime());
+            assert.equal(date2.getTime(), new Date(2016, 0, 2, 0, 0, 0, 0).getTime());
+            assert.equal(date3.getTime(), new Date(2016, 0, 3, 0, 0, 0, 0).getTime());
+            assert.equal(date4.getTime(), new Date(2016, 0, 4, 0, 0, 0, 0).getTime());
+            assert.equal(date5.getTime(), new Date(2016, 0, 5, 0, 0, 0, 0).getTime());
+            
+            expect(function () {
+                
+                dateType.getValue(model);
+                
+            }).to.throw(Error, 'Max increment. Add cycle: true to allow reloop');
+            
+        });
+    });
+});
